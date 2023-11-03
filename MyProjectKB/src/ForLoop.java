0a89a848a14ce9e04787fc0e4586cc922afaf509
KayiraBertrand <96463282+kaybertr@users.noreply.github.com>
2023-11-03 11:10:32 +0200

    Delete MyProjectKB directory

--- MyProjectKB/src/ForLoop.java
+++
@@ -1,9 +0,0 @@
-public class ForLoop {
-    public static void main(String[] args) throws Exception {
-        for(int i=10; i>=1; i--) {
-            System.out.print(i + " "); 
-          } 
-          
-    }
-    
-}
