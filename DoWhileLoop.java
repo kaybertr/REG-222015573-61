b43701ff530ce15e9db562e1f78071375115ba41
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:37:07 +0200

    DoWhileLoop.java

--- DoWhileLoop.java
+++ DoWhileLoop.java
@@ -1,14 +1,21 @@
 public class DoWhileLoop {
-    public static void main(String[] args) {
-         
-        String text = "Hello";
-        int i = 0;
+
+  public static void main(String[] args) {
+
+    String text = "Hello";
     
-        do {
-          System.out.println(text.charAt(i));
-          i++;
-        } while(i < text.length());
+    int i = 0;
     
-      }
+    // Execute loop body at least once
+    do {
     
+      // Print current char from text
+      System.out.println(text.charAt(i));  
+      
+      // Increment i after each iteration
+      i++;
+      
+    // Continue looping while i is less than text length  
+    } while(i < text.length());
+  }
 }
