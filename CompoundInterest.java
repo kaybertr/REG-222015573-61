a10397da1d6c3c7929aad36333447e72e5125063
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:36:27 +0200

    CompoundInterest.java

--- CompoundInterest.java
+++ CompoundInterest.java
@@ -2,27 +2,35 @@ public class CompoundInterest {
 
   public static void main(String[] args) {
 
-    Loan loan = new Loan(500000, 18); 
-    double total = loan.calculateTotalAmount(3);
-
+    // Create loan with 500000 principal, 18% rate  
+    Loan loan = new Loan(500000, 18);
+    
+    // Calculate total amount after 3 years
+    double total = loan.calculateTotalAmount(3);  
+    
+    // Print total amount
     System.out.println("The total amount paid after 3 years is: " + total);
-
-  }
-
+  } 
 }
 
+// Loan class
 class Loan {
 
   double principal;
   double rate;
-
+  
+  // Constructor to initialize values
   public Loan(double p, double r) {
     principal = p;
     rate = r/100; 
   }
 
+  // Calculate total amount with compound interest  
   public double calculateTotalAmount(int years) {
+    
+    // Calculate compound interest 
     double total = principal * Math.pow(1 + rate/12, years*12);
+    
     return total;
   }
 
