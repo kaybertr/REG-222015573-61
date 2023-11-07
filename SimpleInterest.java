2eb170ba9cd7f7734c68254000aa207129524eaf
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:40:04 +0200

    SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,23 +1,29 @@
 public class SimpleInterest {
 
   public static void main(String[] args) {
-    
-    BankAccount account = new BankAccount(100000);
+  
+    // Create a BankAccount with 100000 principal
+    BankAccount account = new BankAccount(100000);  
+
+    // Calculate interest for 5 years at 5% rate
     double interest = account.calculateInterest(5, 5);
     
+    // Print the simple interest earned
     System.out.println("The simple interest earned is: " + interest);
   }
-
 }
 
+// BankAccount class 
 class BankAccount {
 
   double principal;
-
+  
+  // Constructor to initialize principal
   public BankAccount(double p) {
-    principal = p;
+    principal = p; 
   }
 
+  // Method to calculate simple interest
   public double calculateInterest(int years, double rate) {
     return principal * years * rate / 100;
   }
