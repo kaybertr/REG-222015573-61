307586a2ea582de4ebccd78da50faa7710aa67e8
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:35:48 +0200

    BrkStatement.java

--- BrkStatement.java
+++ BrkStatement.java
@@ -1,11 +1,17 @@
 public class BrkStatement {
-    public static void main(String[] args) throws Exception {
-        for(int i=1; i<=5; i++){
-            if(i == 3){
-              break;  
-            }
-            System.out.println(i);
-        }
+
+  public static void main(String[] args) throws Exception {
+
+    // Loop from 1 to 5
+    for(int i=1; i<=5; i++){
+    
+      // Break out of loop if i is 3
+      if(i == 3){
+        break;
+      }
       
+      // Print value of i
+      System.out.println(i);
     }
+  }
 }
