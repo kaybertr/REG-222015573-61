f53ea6f3b80e0221f49891bae8b8d72de58e5a18
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:38:20 +0200

    IF.java

--- IF.java
+++ IF.java
@@ -1,17 +1,37 @@
 public class IF {
+
   public static void main(String args[]){
+    
     int age = 18;
+    
+    // Check if age is less than 18
     if (age < 18 ) {
-      System.out.println("You're allowed 60% Discount");
-    }else if( age >= 18 && age < 25){
+      System.out.println("You're allowed 60% Discount"); 
+    }
+    
+    // Check if age is between 18 and 25
+    else if( age >= 18 && age < 25){
       System.out.println("You're allowed 40% Discount");
-    }else if ( age >= 25 && age < 35){
+    }
+    
+    // Check if age is between 25 and 35
+    else if ( age >= 25 && age < 35){
       System.out.println("You're allowed 35% Discount");
-    }else if(age >=35 && age < 40){
+    }
+    
+    // Check if age is between 35 and 40  
+    else if(age >=35 && age < 40){
       System.out.println("You're allowed 30% Discount");
-    }else if(age >= 40 && age < 45){
+    }
+    
+    // Check if age is between 40 and 45
+    else if(age >= 40 && age < 45){
       System.out.println("You're allowed 25% Discount");
-    }else{
+    }
+    
+    // If none of the above conditions are met
+    else{
       System.out.println("You're not allowed Discount!");
     }
-    }} 
+  }
+}
