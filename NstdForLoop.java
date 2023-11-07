3205437f5158ced509d40d7e0445c63e4288e75d
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:38:57 +0200

    NstdForLoop.java

--- NstdForLoop.java
+++ NstdForLoop.java
@@ -1,17 +1,21 @@
 public class NstdForLoop {
 
   public static void main(String[] args) {
+  
+    // Outer loop to control rows
     for(int i=1; i<=3; i++) {
-      System.out.print("Row " + i + ": "); 
+    
+      System.out.print("Row " + i + ": ");
       
-      
+      // Inner loop to print columns  
       for(int j=1; j<=5; j++) {
+      
+        // Print column number
         System.out.print(j + " ");
       }
       
+      // Move to next line after each row
       System.out.println(); 
     }
-
   }
-
 }
