613c5cb1e8a4f08fe634bbbbb40a0f53be02bdf0
KayiraBertrand <96463282+kayirabertrand@users.noreply.github.com>
2023-11-07 10:39:33 +0200

    SWITCH.java

--- SWITCH.java
+++ SWITCH.java
@@ -1,17 +1,30 @@
 public class SWITCH{
-  public static void main(String args[]){
-   int taxifees= 350;
-   switch (taxifees) {
-    case 200: System.out.println("Kagugu to Kimironko");
-      
-      break;
-      case 300,350: System.out.println("Kimironko to Remera");
 
-      break;
-      case 400,450: System.out.println("Remera to Kicukiro");
-      break;
-   
-    default: System.out.println("Kigali to Province");
-   } 
-  } 
+  public static void main(String args[]){
+
+    int taxifees= 350;
+    
+    // Switch based on value of taxifees
+    switch (taxifees) {
+    
+      // If 200, print route
+      case 200: 
+        System.out.println("Kagugu to Kimironko");
+        break;
+        
+      // If 300 or 350, print route  
+      case 300,350:
+        System.out.println("Kimironko to Remera");
+        break;
+        
+      // If 400 or 450, print route
+      case 400,450:
+        System.out.println("Remera to Kicukiro");
+        break;
+        
+      // Default case  
+      default:
+        System.out.println("Kigali to Province"); 
+    }
+  }
 }
